ca24a16743b01e5ead261b42e9ee521b696b719b
Charlie Gaines <beaugaines@gmail.com>
2016-09-16 14:35:04 -0400

    timer and short, long breaks working

--- app/assets/javascripts/pomodoro_app.js
+++ app/assets/javascripts/pomodoro_app.js
@@ -3,21 +3,51 @@ var app = angular.module('pomodoroTimer', ['templates']);
 app.controller('TimerCtrl', [
             '$scope', '$http', '$interval',
     function($scope, $http, $interval) {
-      $scope.timerStarted = null;
-      $scope.countDown = 1500;
 
-      $scope.startTimer = function () {
-        if ($scope.timerStarted) {
-          $scope.timerStarted = false;
-        } else {
-          $scope.timerStarted = true;
-          $interval(updateCountdown, 1000);
-        }
+      $scope.sessionCount = 0;
+
+
+      $scope.startTimer = function (seconds) {
+        $scope.countDown = Number(seconds);
+        $scope.timerRunning = true;
+        timer = $interval(updateCountdown, 1000);
       };
 
       function updateCountdown() {
         $scope.countDown--;
+
+        if ($scope.countDown === 0) {
+          $interval.cancel(timer);
+          timer = undefined;
+          $scope.timerRunning = false;
+
+          if ($scope.shortBreak || $scope.longBreak) {
+            $scope.shortBreak = false;
+            $scope.longBreak = false;
+            $scope.working = true;
+          } else {
+            $scope.sessionCount++;
+            $scope.working = false;
+            if ($scope.sessionCount % 4 === 0) {
+              $scope.longBreak = true;
+            } else {
+              $scope.shortBreak = true;
+            }
+          }
+        }
       }
+
+      $scope.pauseTimer = function () {
+        // how to pause $interval?
+      };
+
+      $scope.resetTimer = function () {
+        $scope.timerRunning = null;
+        $scope.countDown = 1500;
+        $interval.cancel(timer);
+        timer = undefined;
+      }
+
     }
 ]);
 
@@ -38,4 +68,20 @@ app.filter('formatTime', function () {
     return output;
   };
 
+});
+
+
+app.directive('countdownTimer', function () {
+  return {
+    restrict: 'E',
+    scope: {
+      count: "="
+    },
+    template: "<div class='timer'>{{count | formatTime}}</div>",
+    // link: function (scope, element, attrs) {
+    //   scope.$watch('count', function (newVal, old) {
+    //     console.log(newVal);
+    //   });
+    // }
+  };
 });
